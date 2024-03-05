e120cf430f045bc3fdd7df76b495c4fde4716082
George Christodoulou <gchris@hotmail.co.uk>
2024-03-05 13:15:23 +0200

    cors fix

--- server.js
+++ server.js
@@ -28,8 +28,14 @@ app.use((req, res, next) => {
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
